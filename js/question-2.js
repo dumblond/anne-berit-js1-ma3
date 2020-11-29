79463f538c777004649f1c0f880131babbb1b0f8
Anne-Berit Murberg <anne-beritm@hotmail.com>
2020-11-29 09:15:58 +0100

    finished

--- js/question-2.js
+++ js/question-2.js
@@ -1,13 +1,11 @@
-const url = "https://api.rawg.io/api/gamess?dates=2019-01-01,2019-12-31&ordering=-rating";
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 
 const dataContainer = document.querySelector(".games");
 
 async function games() {
-
     try {
-
         const response = await fetch(url);
-    
+
         const results = await response.json();
 
         console.log(results);
@@ -17,10 +15,6 @@ async function games() {
         dataContainer.innerHTML = "";
 
         for (let i = 0; i < facts.length; i++) {
-            console.log(facts[i].name);
-            console.log(facts[i].rating);
-            console.log(facts[i].tags.length)
-
             if (i === 8) {
                 break;
             }
@@ -30,11 +24,11 @@ async function games() {
                 <h2>${facts[i].name}</h2>
                 <p>Rating: ${facts[i].rating}</p>
                 <p>Number of tags: ${facts[i].tags.length}</p>
-            </div>`
+            </div>`;
         }
     } catch (error) {
         console.log("Something went wrong");
-        dataContainer.innerHTML = displayError("Something is wrong")
+        dataContainer.innerHTML = displayError("Something is wrong");
     }
 }
 
